0b3d2d47913edbd747e75b1d4f1cbe5ed41e086f
Jonathan Lewis <futbolpal@gmail.com>
2014-05-23 21:10:06 -0400

    Removed ActiveRecord dependencies

--- app/models/airing.rb
+++ app/models/airing.rb
@@ -3,7 +3,7 @@ class Airing
 
 	attr_accessor :ProgramId,:SeriesId,:InstanceId,:UnknownInstance,:UPId,:Title,:EpisodeTitle,:Copy
 	attr_accessor :AiringTime,:Duration,:Color,:AiringType,:ShowingType,:CC,:LetterBox,:Stereo,:HD,:SAP
-	attr_accessor :TVRating,:Dolby,:DSS,:HDLevel,:Sports,:DVS,:Category,:Subcategory,:SourceId,:Channel
+	attr_accessor :TVRating,:MovieRating,:Dolby,:DSS,:HDLevel,:Sports,:DVS,:Category,:Subcategory,:SourceId,:Channel
 	attr_accessor :CallLetters,:SourceDisplayName,:SourceLongName,:TiVoSupported,:ParentNetworkId
 	attr_accessor :IconAvailable,:ServiceId,:DvbTriplet,:SL,:Subtitles
 	alias_method :id, :ProgramId
